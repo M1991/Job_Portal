2f91884e1772e840a4e48dafbf001e84159dfc89
Manoj <manoj.haloi8@gmail.com>
2025-07-19 15:55:17 +0530

    Swagger Docs

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -58,11 +58,65 @@ const router = express.Router();
  *        location: mumbai
  */
 
+/**
+ *  @swagger
+ *  tags:
+ *    name: Auth
+ *    description: authentication apis
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/register:
+ *    post:
+ *      summary: register new user
+ *      tags: [Auth]
+ *      requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *      responses:
+ *        200:
+ *          description: user created successfully
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ *        500:
+ *          description: internal serevr error
+ */
+
+
 // routes
 
 // REGISTER || POST
 router.post('/register', limiter, registerController);
 
+
+/**
+ * @swagger
+ * /api/v1/auth/login:
+ *  post:
+ *    summary: login page
+ *    tags: [Auth]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *    responses:
+ *      200:
+ *        description: login successfull
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *      500:
+ *        description: something went wrong
+ */
 // LOGIN || POST
 router.post('/login', limiter, loginController);
 
